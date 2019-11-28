88ce908187425d8e68807537007d9e8706be2c1f
zhouhuan <2296732382@qq.com>
2019-11-28 13:44:02 +0800

    提交

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -9,23 +9,23 @@ class CardController extends Controller
     {
         $name = $request->name;                 // 姓名
         $sex = $request->sex;                   // 性别
-        $ID_crad = $request->num;               // 身份证号码
+        $ID_card = $request->num;               // 身份证号码
         $nation = $request->nationality;        // 民族
         $home = $request->address;              // 住址
         $music = $request->music;
 //        echo $name;
 //        echo $sex;
-//        echo $ID_crad;
+//        echo $ID_card;
 //        echo $nation;
 //        echo $home;
 //        echo $music;
-        if(!empty($name) && !empty($ID_crad)){
-            $sql = DB::table('IDcard')->where('ID_crad', $ID_crad)->first();
+        if(!empty($name) && !empty($ID_card)){
+            $sql = DB::table('IDcard')->where('ID_card', $ID_card)->first();
             if ($sql){
 //                echo $sql->created_at;
                     $data = [
                         'data' => [
-                            'crad' => $sql->ID_crad,
+                            'card' => $sql->ID_card,
                             'name' => $sql->name,
                             'music' => $sql->music,
                             'createtime' => $sql->created_at,
@@ -35,7 +35,7 @@ class CardController extends Controller
                 return response()->json($data,201);
 //                return response()->json(['message'=>'身份信息已存在是否继续添加', 'code'=> -1, 'status_code'=>422],200);
             }else{
-                DB::table('IDcard')->insert(['ID_crad'=>$ID_crad,'sex'=>$sex,'name'=>$name,'home'=>$home,'nation'=>$nation, 'music'=>$music]);
+                DB::table('IDcard')->insert(['ID_card'=>$ID_card,'sex'=>$sex,'name'=>$name,'home'=>$home,'nation'=>$nation, 'music'=>$music]);
 //            $sql = DB::table('IDcard')->where('id',352)->get();
                 return response()->json('ok',200);
             }
@@ -47,12 +47,12 @@ class CardController extends Controller
     public function create(Request $request){
         $name = $request->name;                 // 姓名
         $sex = $request->sex;                   // 性别
-        $ID_crad = $request->num;               // 身份证号码
+        $ID_card = $request->num;               // 身份证号码
         $nation = $request->nationality;        // 民族
         $home = $request->address;              // 住址
         $music = $request->music;
 
-        DB::table('IDcard')->insert(['ID_crad'=>$ID_crad,'sex'=>$sex,'name'=>$name,'home'=>$home,'nation'=>$nation, 'music'=>$music]);
+        DB::table('IDcard')->insert(['ID_card'=>$ID_card,'sex'=>$sex,'name'=>$name,'home'=>$home,'nation'=>$nation, 'music'=>$music]);
         return response()->json('ok',200);
 
     }
